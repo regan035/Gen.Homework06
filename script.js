6ec5a6537e610fa118fb0f6dfa4280edd5bd2cea
regan035 <regan035@gmail.comgit config --global user.name regan035git statusgit config --global user.email regan035@gmail.com>
2020-02-05 13:07:58 -0600

    update js

--- script.js
+++ script.js
@@ -9,25 +9,28 @@ var cityList = $("#city-list");
 
 
 
-$("#search").click(function(result){
+$("#search").click(function(event){
 
-    var cityEl = $("#city").val();
+    var cityEl = $("#city").val().replace(/ /g, "+");
     var APIkey = "3744579bd802d039a5031e6ef3051381";
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityEl + "&units=imperial&appid=" + APIkey;
+    var queryURLcurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + cityEl + "&units=imperial&appid=" + APIkey;
 
     $.ajax({
-        url: queryURL,
+        url: queryURLcurrent,
         method: "GET"
+    }). then(function(result){
+        console.log(result);
+        $(".name").html("<h4>" + cityEl + " Weather Details</h4>");
+        $(".temp").text("Temperature(F): " + result.main.temp);
+        $(".humidity").text("Humidity: " + result.main.humidity);
+        $(".speed").text("Temperature (F) " + result.wind.speed);
     });
     console.log(cityEl); 
-    console.log(queryURL);
-    console.log(result);
+    console.log(queryURLcurrent);
+    console.log(event);
     event.preventDefault();
 
-    $(".name").html("<h4>" + cityEl + " Weather Details</h4>");
-    $(".temp").text("Temperature(F): " + result.main.temp);
-    $(".humidity").text("Humidity: " + result.main.humidity);
-    $(".speed").text("Temperature (F) " + result.wind.speed);
+   
 })
 
 // })
